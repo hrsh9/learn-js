663639dc1ca514a83e73c8bd1261ff2b8203fbfd
hrsh9 <harshsinha.1203@gmail.com>
2023-12-21 03:25:55 +0530

    variables

---
+++ 02.Variables/const.js
@@ -0,0 +1,10 @@
+const accountID = 58594 //can't be changed
+let accountEmail = "harsh@gmail.com" //
+var accountPassword = "akdjhf349857"
+accountCity = "jaipur" //not recommended to declare variable like this but remember it is possible
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
+
+//javascript has no block-scope and functional-scope in initial days because of var
+//so var should no be used in today's time
+//rather we should use 'let' & 'const'
