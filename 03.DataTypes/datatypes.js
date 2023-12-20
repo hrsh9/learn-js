a9dab0cf466f395db9a6cd8c8e506f639597e44a
hrsh9 <harshsinha.1203@gmail.com>
2023-12-21 03:39:46 +0530

    datatypes

---
+++ 03.DataTypes/datatypes.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as newer version by jsEngine
+
+//js engine is hidden inside browser
+
+// https://tc39.es/ecma262/#sec-intro & MDN docs 
+
+/*
+    Data types in js:-
+    
+    primitive data type
+    1) Number
+    2) Bigint
+    3) String
+    4) Boolean
+    5) null : standalone value | representation of empty value
+    6) undefined 
+    7) symbol
+
+
+    1) object
+    2) 
+
+*/
