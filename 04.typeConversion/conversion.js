60170bb6035435a05f7afeed2c53803522413ce0
hrsh9 <harshsinha.1203@gmail.com>
2023-12-21 10:29:32 +0530

    conversion

---
+++ 04.typeConversion/conversion.js
@@ -0,0 +1,16 @@
+let score = "33";
+console.log(typeof score);
+console.log(typeof(score));
+console.log(typeof(Number(typeof(score))));
+
+let convertedToNum = Number(score);
+console.log(typeof(convertedToNum));
+console.log(convertedToNum);
+ 
+
+/*For conversion
+int -> Number
+string -> String
+boolean -> Boolean
+...
+*/
