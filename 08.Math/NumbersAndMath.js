ba8c7709553aaa112403e5a47c58a806d1bba15b
hrsh9 <harshsinha.1203@gmail.com>
2023-12-21 16:43:02 +0530

    math

---
+++ 08.Math/NumbersAndMath.js
@@ -0,0 +1,58 @@
+let score = 400;
+console.log(score);
+
+let balance = new Number(300);
+console.log(balance);
+
+console.log(balance.toString().length);
+
+//------------Math----------------------
+console.log(Math);
+
+/*
+E: 2.718281828459045
+LN2: 0.6931471805599453
+LN10: 2.302585092994046
+LOG2E: 1.4426950408889634
+LOG10E: 0.4342944819032518
+PI: 3.141592653589793
+SQRT1_2: 0.7071067811865476
+SQRT2: 1.4142135623730951
+abs: ƒ abs()
+acos: ƒ acos()
+acosh: ƒ acosh()
+asin: ƒ asin()
+asinh: ƒ asinh()
+atan: ƒ atan()
+atan2: ƒ atan2()
+atanh: ƒ atanh()
+cbrt: ƒ cbrt()
+ceil: ƒ ceil()
+clz32: ƒ clz32()
+cos: ƒ cos()
+cosh: ƒ cosh()
+exp: ƒ exp()
+expm1: ƒ expm1()
+floor: ƒ floor()
+fround: ƒ fround()
+hypot: ƒ hypot()
+imul: ƒ imul()
+log: ƒ log()
+log1p: ƒ log1p()
+log2: ƒ log2()
+log10: ƒ log10()
+max: ƒ max()
+min: ƒ min()
+pow: ƒ pow()
+random: ƒ random()
+round: ƒ round()
+sign: ƒ sign()
+sin: ƒ sin()
+sinh: ƒ sinh()
+sqrt: ƒ sqrt()
+tan: ƒ tan()
+tanh: ƒ tanh()
+trunc: ƒ trunc()
+*/
+
+//Math.random()
