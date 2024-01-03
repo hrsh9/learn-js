8ca68ad52baf0016b3d20a17935044b75fda4408
hrsh9 <harshsinha.1203@gmail.com>
2024-01-03 13:18:34 +0530

    function

---
+++ 02.Basics-part2/03.function.js
@@ -0,0 +1,18 @@
+// function name(){
+//     console.log("H");
+//     console.log("A");
+//     console.log("R");
+//     console.log("S");
+//     console.log("H");
+// }
+//name();
+
+function addition(num1, num2){
+    return num1 + num2;
+}
+
+let res = addition(5, 8);
+let strRes = addition(5, '8');
+console.log(res);
+console.log(strRes);
+
