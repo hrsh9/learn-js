cda2f9423a88ab1446ad7bc13e679d3dad3323b0
hrsh9 <harshsinha.1203@gmail.com>
2024-01-04 19:00:09 +0530

    ahof

---
+++ 03.controlFlow/05.arrHigherOrderFn.js
@@ -0,0 +1,24 @@
+const n = [1,2,3,4,5,6,7,8,9];
+
+//FILTER
+
+// const newN = n.filter((i)=>(i > 4))
+// console.log(newN);
+
+// console.log(n.filter((i)=>(i > 4)))
+
+// const N = [];
+// n.forEach((num)=>{
+//     if(num > 4){
+//         N.push(num);
+//     }
+// })
+// console.log(N);
+
+
+
+//MAP
+//chaining : map().map().filter()
+
+
+//REDUCE
