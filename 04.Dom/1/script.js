58488aaf83131459d408b36681bab1b2a7c1abfc
hrsh9 <harshsinha.1203@gmail.com>
2024-01-06 08:26:14 +0530

    dom2

---
+++ 04.Dom/1/script.js
@@ -0,0 +1,32 @@
+/*
+in console of browser
+console.log(window)
+console.log(document) || console.log(window.document)
+c.l(document.link)
+
+*/
+
+/**
+ * document.getElementById("")
+ * document.getElementById("").innerHTML
+ * document.getElementById("").getAttribute("")
+ * 
+ * document.getElementByClassName("")
+ * 
+ * document.querySelector('tag') | ('#id') | ('.classname')
+ * document.querySelector('input[type="password"])
+ * 
+ * document.querySelectorAll('li')
+ */
+
+/**
+ * title.style.backgroundColor = "red"
+ * title.style.borderRadius = "5px"
+ * 
+ */
+
+/**
+ * to convert in array
+ * const convertedArray = Array.from(HTMLcollection)
+ */
+
