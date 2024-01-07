305c75da7cb30fa1d9af3fd845d6a7f3ebb1b453
hrsh9 <harshsinha.1203@gmail.com>
2024-01-08 01:38:25 +0530

    clock

---
+++ projects/DigitalClock/script.js
@@ -0,0 +1,7 @@
+// document.querySelector("#clock")
+const clock = document.getElementById('clock');
+
+setInterval(() => {
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString();
+},1000);
